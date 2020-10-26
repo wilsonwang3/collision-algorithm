5e0f0c0e9f7a5d416a550f21e3f4edfcfb5a50ac
wilsonwang3 <73449867+wilsonwang3@users.noreply.github.com>
2020-10-25 18:08:03 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,400);
+
+  fixedRect = createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background(255,255,255);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x-movingRect.x < fixedRect.width/2 + movingRect.width/2
+      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y-movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+
+  } else {
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+
+  }
+  drawSprites();
+
+  
+}
